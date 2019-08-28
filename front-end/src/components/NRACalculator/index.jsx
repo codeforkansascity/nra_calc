cd0693febc486fa742c5bf8088c1355acf4633fb
blakebford3 <blakebford3@gmail.com>
2019-08-27 23:58:07 -0500

    initial commit

--- front-end/src/components/NRACalculator/index.jsx
+++ front-end/src/components/NRACalculator/index.jsx
@@ -1,14 +1,65 @@
 import React from 'react';
 import './NRACalculator.scss';
+//import for Semantic-UI components
+import { Button, Form, Grid, Segment, GridColumn } from 'semantic-ui-react'
+
+
 
 class NRACalculator extends React.Component {
-  render = () => {
+  constructor(props) {
+    super(props)
+    this.state = {
+      current: "",
+      improvements: "",
+      zone: ""
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleCurrent = e => {
+    this.setState({
+      current: e.target.value,
+    })
+  }
+
+  handleImprovements = e => {
+    this.setState({
+      improvements: e.target.value,
+    })
+  }
+
+  handleZone = e => {
+    this.setState({
+      zone: e.target.value,
+    })
+  }
+//will pass input values from state to backend as object
+  handleSubmit() {
+    console.log(this.state)
+  }
+
+  render() {
     return (
-      <div>
-        <h2>Calculator goes here?</h2>
-      </div>
-    );
+      <Grid textAlign='center'>
+        <GridColumn style={{ maxWidth: 500 }}>
+          <Form size='large'>
+                <Segment stacked>
+                  <Form.Input placeholder='Current Value' onChange={this.handleCurrent}/>
+                  <Form.Input placeholder='Improvements Value' onChange={this.handleImprovements}/>
+                  <Form.Input placeholder='Zone' onChange={this.handleZone}/>
+                  <Button color='blue' fluid size='large' onClick={this.handleSubmit}>
+                    Calculate Rebate
+                  </Button>
+                </Segment>
+          </Form>
+        </GridColumn>
+      </Grid>
+    )
   }
 }
+
+
+
+
 
 export default NRACalculator;
