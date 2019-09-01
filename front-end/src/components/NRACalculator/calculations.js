75e018a5725c4ae3ac7e378f665de41a9bdacb64
Matt Cook <matt@mattcook.xyz>
2019-08-31 21:31:18 -0500

    Add calculations per excel spreadsheet provided by Jud

---
+++ front-end/src/components/NRACalculator/calculations.js
@@ -0,0 +1,65 @@
+// TODO: What is this value from the excel spreadsheet?
+// Assessment ratio?
+const assessmentRatio = 0.115;
+
+export const getNRAEstimates = (startingValue, estValueAfterImprovements, zone) => {
+
+    const { millRateHigh, millRateLow, millRateAvg, incentiveYears } = getZoneData(zone);
+
+    // Calculate based on high, low, and average mill rate within given zone
+    // Eventually we hope to create a more granular estimate based on address,
+    // but as a first pass, this removes a lot of complexity
+    const estHigh = calculateRebate(startingValue, estValueAfterImprovements, millRateHigh, incentiveYears);
+    const estLow = calculateRebate(startingValue, estValueAfterImprovements, millRateLow, incentiveYears);
+    const estAverage = calculateRebate(startingValue, estValueAfterImprovements, millRateAvg, incentiveYears);
+
+    return {
+        estHigh,
+        estLow,
+        estAverage,
+    };
+
+}
+
+// Calculate NRA tax incentive
+export const calculateRebate = (startingValue, estValueAfterImprovements, millRate, incentiveYears) => {
+
+    // Calculate current taxes
+    const currentAssessedValue = startingValue * assessmentRatio;
+    const currentTaxes = calculateTaxes(currentAssessedValue, millRate);
+
+    // Calculate new taxes
+    const fivePercentImprovements = (estValueAfterImprovements - startingValue) * 0.05;
+    const newTaxableAmount = startingValue + fivePercentImprovements;
+    const newTaxableAmountAssessed = newTaxableAmount * assessmentRatio;
+    const newTaxes = calculateTaxes(newTaxableAmountAssessed, millRate);
+
+    // Calculate incremental tax values
+    const incrementalAppraisedValue = estValueAfterImprovements - fivePercentImprovements;
+    const incrementalAssessedValue = incrementalAppraisedValue * assessmentRatio;
+    const incrementalTaxSavings = (incrementalAssessedValue / 1000) * millRate;
+
+    // Calculate payments/savings per year
+    const payPerYear = newTaxes;
+    const savePerYear = incrementalTaxSavings;
+
+    // Total savings
+    const savings = savePerYear * incentiveYears;
+    
+    // Several values that might be useful to display
+    return {
+        currentTaxes,
+        newTaxes,
+        incrementalTaxSavings,
+        payPerYear,
+        savePerYear,
+        savings
+    }
+
+}
+
+export const calculateTaxes = (amount, millRate) => {
+
+    return ((amount / 1000) * millRate) - 44;
+
+}
