41e83de68ab0fcd9d0ba6dedff4cb6f8e18acd59
Matt Cook <matt@mattcook.xyz>
2019-09-07 14:55:04 -0500

    Try github pages build

---
+++ docs/precache-manifest.4c4dc6ec4431a12b9944816859a5097f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af180d25c569ddf8d232adbd648f0b04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c266f734634d83499ef9",
+    "url": "/static/css/main.35070955.chunk.css"
+  },
+  {
+    "revision": "81e6b2c7e8cae9d73dbe",
+    "url": "/static/js/2.8f10746e.chunk.js"
+  },
+  {
+    "revision": "c266f734634d83499ef9",
+    "url": "/static/js/main.18629dff.chunk.js"
+  },
+  {
+    "revision": "f03f84ac943d56975aaa",
+    "url": "/static/js/runtime~main.2a6ff9c3.js"
+  }
+]);
