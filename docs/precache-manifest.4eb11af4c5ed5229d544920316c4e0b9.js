b2624378573ccb8db40d07f4c55ddec3f77a95ff
Matt Cook <matt@mattcook.xyz>
2019-09-07 14:57:36 -0500

    Attempted fix

---
+++ docs/precache-manifest.4eb11af4c5ed5229d544920316c4e0b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7193b007c8db9c0c22c9d8bc7df7be80",
+    "url": "/nra_calc/index.html"
+  },
+  {
+    "revision": "4d4820852fefaa5d0070",
+    "url": "/nra_calc/static/css/main.35070955.chunk.css"
+  },
+  {
+    "revision": "81e6b2c7e8cae9d73dbe",
+    "url": "/nra_calc/static/js/2.8f10746e.chunk.js"
+  },
+  {
+    "revision": "4d4820852fefaa5d0070",
+    "url": "/nra_calc/static/js/main.a6bafcb4.chunk.js"
+  },
+  {
+    "revision": "bdd57971d7803e1ac1e7",
+    "url": "/nra_calc/static/js/runtime~main.fbc4a88b.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c4dc6ec4431a12b9944816859a5097f.js"
+  "/nra_calc/precache-manifest.4eb11af4c5ed5229d544920316c4e0b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nra_calc/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
