f2f8cdc69cec2aa20ad8ea6c42f50aa5859fe7c4
blakebford3 <blakebford3@gmail.com>
2019-09-08 01:28:54 -0500

    Added zone data to map as GeoJSON

--- front-end/src/components/Map/index.js
+++ front-end/src/components/Map/index.js
@@ -1,32 +1,48 @@
 import React from 'react'
-import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map as LeafletMap, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
 
-const position = [39.1155, -94.6268];
 
 class Map extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      polygons: [],
+      position: [39.1155, -94.6268],
+      zoom: 12,
+    }
+  }
 
   test = (input) => {
       console.log(input);
+  }
+
+  //load geoJSON containing zone polygons
+  async componentDidMount() {
+    //API call
+    const response = await fetch(`https://opendata.arcgis.com/datasets/adb3297a0cb14d32b0274aaffa469538_0.geojson`)
+    const json = await response.json();
+    this.setState({
+      polygons: json,
+    })
+    console.log(this.state.polygons)
   }
 
   render = () => {
     return (
       <LeafletMap
         style={{ width: '100%' }}
-        center={position}
-        zoom={12}
-        maxZoom={16}
-        attributionControl={true}
-        zoomControl={true}
-        doubleClickZoom={true}
-        scrollWheelZoom={true}
-        dragging={true}
-        animate={true}
-        easeLinearity={0.2}
+        center={this.state.position}
+        zoom={this.state.zoom}
       >
-        <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' />
+        <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
+
+        <GeoJSON 
+        key={this.state.polygons}
+        data={this.state.polygons}
+        />
+        
         {/* setZoneFromMap sets the zone in the NRACalculator component */}
-        <Marker position={position} onClick={this.props.setZoneFromMap}>
+        <Marker position={this.state.position} onClick={this.props.setZoneFromMap}>
           <Popup open>Successfully set your zone to 1</Popup>
         </Marker>
       </LeafletMap>
