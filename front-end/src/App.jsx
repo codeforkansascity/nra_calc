379b5b7e993c77dc18d9ab73436c196e4cf661fa
Filipe <filipegomes404@gmail.com>
2019-09-10 20:54:28 -0500

    Added tabs

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,34 +3,36 @@ import AppHeader from './components/AppHeader';
 import './App.scss';
 import AppFooter from './components/AppFooter';
 import NRACalculator from './components/NRACalculator';
-import Tabs from './Tabs';
-require('./tab_styles.css');
+import Tabs from './components/Tabs/Tabs';
+require('./components/Tabs/Tabs.css');
 
 function App() {
   return (
     <div className="App">
       <AppHeader>Neighborhood Revitalization Act Calculator</AppHeader>
-      <p>
-        This is an effort by the Unified Government of Wyandotte County and 
-        Kansas City, Kansas to promote revitalization of designated areas through
-        tax incentives on new construction and improvements. Start below to estimate your potential tax incentive.
-      </p>
-      <NRACalculator></NRACalculator>
+      <Tabs>
+        <div label="Calculator">
+        <p>
+          This is an effort by the Unified Government of Wyandotte County and 
+          Kansas City, Kansas to promote revitalization of designated areas through
+          tax incentives on new construction and improvements. Start below to estimate your potential tax incentive.
+        </p>
+        <NRACalculator></NRACalculator>
+        </div>
+        <div label="About">
+          Information about the Neighborhood Revitalization Act.
+        </div>
+        <div label="Resources">
+          Helpful links to redirect users to additional material, application forms, etc.
+        </div>
+        <div label="Contact Us">
+          Contact information, links, emails, for people involved in this project
+        </div>
+      </Tabs>
       <AppFooter>
         Created in partnership with <a href="https://codeforkc.org">Code for KC</a><br />
         Contribute code on <a href="https://github.com/codeforkansascity/nra_calc">Github</a>
       </AppFooter>
-      <Tabs>
-      <div label="About">
-        Information about the Neighborhood Revitalization Act.
-      </div>
-      <div label="Resources">
-        Helpful links to redirect users to additional material, application forms, etc.
-      </div>
-      <div label="Contact Us">
-        Contact information, links, emails, for people involved in this project
-      </div>
-    </Tabs>
     </div>
   );
 }
