274e727d9fd11c33bf345f2073a044c1ceb40c8a
Matt Cook <matt@mattcook.xyz>
2019-10-08 16:10:11 -0700

    Remove Checkbox import since we're using Form.Checkbox

--- front-end/src/components/NRACalculator/index.jsx
+++ front-end/src/components/NRACalculator/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './NRACalculator.scss';
 //import for Semantic-UI components
-import { Button, Icon, Form, Checkbox, Grid, Segment, GridColumn, Label } from 'semantic-ui-react'
+import { Button, Icon, Form, Grid, Segment, GridColumn, Label } from 'semantic-ui-react'
 import { getNRAEstimates } from './calculations';
 import ZonePicker from '../ZonePicker';
 
