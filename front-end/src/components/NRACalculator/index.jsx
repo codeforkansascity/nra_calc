12a2a07642528bd140b0de4f9f998b6581f725ed
Matt Cook <matt@mattcook.xyz>
2019-10-09 17:52:18 -0700

    More linting fixes

--- front-end/src/components/NRACalculator/index.jsx
+++ front-end/src/components/NRACalculator/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/no-unused-state */
 import React from 'react';
 import './NRACalculator.scss';
 // import for Semantic-UI components
@@ -6,6 +8,18 @@ import {
 } from 'semantic-ui-react';
 import { getNRAEstimates } from './calculations';
 import ZonePicker from '../ZonePicker';
+
+function cleanNumber(num) {
+  // Remove $ and , symbols for calculation
+  const cleanNum = num.toString().replace(/,*\$*/g, '');
+  // eslint-disable-next-line radix
+  return parseInt(Math.floor(cleanNum));
+}
+
+function formatNumber(num) {
+  // This will limit to 2 decimals and add commas to thousands
+  return num.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+}
 
 // array of property type options for drop-down input
 const HomeOptions = [
@@ -77,188 +91,183 @@ class NRACalculator extends React.Component {
   }
 
   handleSubmit() {
-    if (this.state.current && this.state.valueAfterInvestment && this.state.zone) {
+    const { current, valueAfterInvestment, zone } = this.state;
+
+    if (current && valueAfterInvestment && zone) {
       this.setState({
-        estimates: getNRAEstimates(this.state.current, this.state.valueAfterInvestment, this.state.zone),
+        estimates: getNRAEstimates(current, valueAfterInvestment, zone),
       });
     }
   }
 
-  render = () => (
-    <Grid textAlign="left" className="NRACalculator">
-      <GridColumn>
-        <Form size="large">
-          <Segment stacked>
+  render = () => {
+    const {
+      current, propertyType, estimates, valueAfterInvestment, investmentType, zone,
+    } = this.state;
+    return (
+      <Grid textAlign="left" className="NRACalculator">
+        <GridColumn>
+          <Form size="large">
+            <Segment stacked>
 
-            {/* Text inputs grouped together */}
-            <Form.Group
-              widths="equal"
-            >
-              {/** Current valuation form input * */}
-              <Form.Input
-                fluid
-                label="Current Property Value"
-                labelPosition="left"
-                placeholder="Current Property Value"
-                onChange={this.handleCurrent}
+              {/* Text inputs grouped together */}
+              <Form.Group
+                widths="equal"
               >
-                <Label>$</Label>
-                <input />
-              </Form.Input>
-              {/** Est. Value After Investment form input * */}
-              <Form.Input
-                fluid
-                label="Est. Value After Investment"
-                labelPosition="left"
-                placeholder="Est. Value After Investment"
-                onChange={this.handleValueAfterInvestment}
+                {/** Current valuation form input * */}
+                <Form.Input
+                  fluid
+                  label="Current Property Value"
+                  labelPosition="left"
+                  placeholder="Current Property Value"
+                  onChange={this.handleCurrent}
+                >
+                  <Label>$</Label>
+                  <input />
+                </Form.Input>
+                {/** Est. Value After Investment form input * */}
+                <Form.Input
+                  fluid
+                  label="Est. Value After Investment"
+                  labelPosition="left"
+                  placeholder="Est. Value After Investment"
+                  onChange={this.handleValueAfterInvestment}
+                >
+                  <Label>$</Label>
+                  <input />
+                </Form.Input>
+              </Form.Group>
+
+              {/* Dropdown inputs grouped together */}
+              <Form.Group
+                widths="equal"
               >
-                <Label>$</Label>
-                <input />
-              </Form.Input>
-            </Form.Group>
+                {/** Dropdown input for improvement type * */}
+                <Form.Select
+                  fluid
+                  label="Investment Type"
+                  options={ImprovOptions}
+                  placeholder="Investment Type"
+                  onChange={this.handleInvestmentType}
+                />
+                {/** Dropdown input for home type * */}
+                <Form.Select
+                  fluid
+                  label="Building Type"
+                  options={HomeOptions}
+                  placeholder="Building Type"
+                  onChange={this.handlePropertyType}
+                />
+              </Form.Group>
 
-            {/* Dropdown inputs grouped together */}
-            <Form.Group
-              widths="equal"
-            >
-              {/** Dropdown input for improvement type * */}
-              <Form.Select
-                fluid
-                label="Investment Type"
-                options={ImprovOptions}
-                placeholder="Investment Type"
-                onChange={this.handleInvestmentType}
-              />
-              {/** Dropdown input for home type * */}
-              <Form.Select
-                fluid
-                label="Building Type"
-                options={HomeOptions}
-                placeholder="Building Type"
-                onChange={this.handlePropertyType}
-              />
-            </Form.Group>
+              {/* Checkbox to mark historical properties. No functionality yet. */}
+              <Form.Group grouped>
+                <label>Other</label>
+                <Form.Checkbox
+                  label="This is a Historical Property"
+                  onChange={this.handleHistorical}
+                />
+              </Form.Group>
 
-
-            {/* Checkbox to mark historical properties. No functionality yet. */}
-            <Form.Group grouped>
-              <label>Other</label>
-              <Form.Checkbox
-                label="This is a Historical Property"
-                onChange={this.handleHistorical}
-              />
-            </Form.Group>
-
-            {/** Zone picker. Opens map modal * */}
-            <ZonePicker
-              setZoneFromMap={this.setZoneFromMap}
-              message={(
-                <>
-                  <Icon name="map" />
-                  {' '}
-Select Your Zone
-                </>
-)}
-              successColor="green"
-              zone={this.state.zone}
-            />
-
-            {/** Submit button * */}
-            <Button
-              disabled={
-                  !this.state.current
-                  || !this.state.valueAfterInvestment
-                  || !this.state.zone
-                  || !this.state.investmentType
-                  || !this.state.propertyType
-                }
-              color="blue"
-              fluid
-              size="large"
-              onClick={this.handleSubmit}
-            >
-              <Icon name="calculator" />
-                Calculate Rebate
-            </Button>
-
-            { /* Calculation results */ }
-            { this.state.estimates
-                && (
-                <Segment basic textAlign="center">
-                  <p>These estimates provide a range depending on the mill rate, which vary within each zone.</p>
-
-                  <p>
-                    <b>You pay</b>
+              {/** Zone picker. Opens map modal * */}
+              <ZonePicker
+                setZoneFromMap={this.setZoneFromMap}
+                message={(
+                  <>
+                    <Icon name="map" />
                     {' '}
-$
-                    {
-                    formatNumber(this.state.estimates.estLow.payPerYear)
-                    }
-                    {' '}
-- $
-                    {
-                      formatNumber(this.state.estimates.estHigh.payPerYear)
-                    }
-                    {' '}
-in taxes each year
-                  </p>
-
-                  <p>
-                    <b>You save</b>
-                    {' '}
-$
-                    {
-                    formatNumber(this.state.estimates.estLow.savePerYear)
-                    }
-                    {' '}
-- $
-                    {
-                      formatNumber(this.state.estimates.estHigh.savePerYear)
-                    }
-                    {' '}
-in taxes per year
-                  </p>
-
-                  <p>
-                    <b>
-Over
-                      {this.state.estimates.estAverage.incentiveYears}
-                      {' '}
-years, you save
-                    </b>
-                    {' '}
-$
-                    {
-                      formatNumber(this.state.estimates.estLow.savings)
-                    }
-                    {' '}
-- $
-                    {
-                      formatNumber(this.state.estimates.estHigh.savings)
-                    }
-                    {' '}
-in taxes.
-                  </p>
-                </Segment>
+                    Select Your Zone
+                  </>
                 )}
+                successColor="green"
+                zone={zone}
+              />
 
-          </Segment>
-        </Form>
-      </GridColumn>
-    </Grid>
-  )
-}
+              {/** Submit button * */}
+              <Button
+                disabled={
+                    !current
+                    || !valueAfterInvestment
+                    || !zone
+                    || !investmentType
+                    || !propertyType
+                  }
+                color="blue"
+                fluid
+                size="large"
+                onClick={this.handleSubmit}
+              >
+                <Icon name="calculator" />
+                  Calculate Rebate
+              </Button>
 
-function cleanNumber(num) {
-  // Remove $ and , symbols for calculation
-  const cleanNum = num.toString().replace(/,*\$*/g, '');
-  return parseInt(Math.floor(cleanNum));
-}
+              { /* Calculation results */ }
+              { estimates
+                  && (
+                  <Segment basic textAlign="center">
+                    <p>These estimates provide a range depending on the mill rate, which vary within each zone.</p>
 
-function formatNumber(num) {
-  // This will limit to 2 decimals and add commas to thousands
-  return num.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+                    <p>
+                      <b>You pay</b>
+                      {' '}
+                        $
+                      {
+                        formatNumber(estimates.estLow.payPerYear)
+                      }
+                      {' '}
+                        - $
+                      {
+                        formatNumber(estimates.estHigh.payPerYear)
+                      }
+                      {' '}
+                        in taxes each year
+                    </p>
+
+                    <p>
+                      <b>You save</b>
+                      {' '}
+                        $
+                      {
+                        formatNumber(estimates.estLow.savePerYear)
+                      }
+                      {' '}
+                        - $
+                      {
+                        formatNumber(estimates.estHigh.savePerYear)
+                      }
+                      {' '}
+                        in taxes per year
+                    </p>
+
+                    <p>
+                      <b>
+                        Over
+                        {estimates.estAverage.incentiveYears}
+                        {' '}
+                        years, you save
+                      </b>
+                      {' '}
+                        $
+                      {
+                        formatNumber(estimates.estLow.savings)
+                      }
+                      {' '}
+                        - $
+                      {
+                        formatNumber(estimates.estHigh.savings)
+                      }
+                      {' '}
+                        in taxes.
+                    </p>
+                  </Segment>
+                  )}
+
+            </Segment>
+          </Form>
+        </GridColumn>
+      </Grid>
+    );
+  }
 }
 
 export default NRACalculator;
