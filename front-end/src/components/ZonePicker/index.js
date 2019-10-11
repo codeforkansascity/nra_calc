e7614f9dcd3723e0bf1d1956d6cca9fdd631788d
Matt Cook <matt@mattcook.xyz>
2019-10-10 17:00:47 -0700

    Convert 4 spaces to 2

--- front-end/src/components/ZonePicker/index.js
+++ front-end/src/components/ZonePicker/index.js
@@ -3,66 +3,66 @@ import { Button, Modal, Icon } from 'semantic-ui-react'
 import Map from '../Map'
 
 const zonePickerStyle = {
-    textAlign: 'center',
-    marginBottom: '1em'
+  textAlign: 'center',
+  marginBottom: '1em'
 }
 
 //popup modal containing Leaflet.js map
 
 class ZonePicker extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { 
-            modalOpen: false 
-        }
+  constructor(props) {
+    super(props)
+    this.state = { 
+      modalOpen: false 
     }
+  }
 
-    handleOpen = () => {
-        this.setState({
-            modalOpen: true
-        })
-    }
-    handleClose = () => {
-        this.setState({
-            modalOpen: false
-        })
-    }
-    render() {
-        return(
-            <div style={zonePickerStyle}>
-                <Modal 
-                trigger={
-                    <Button 
-                        fluid size='large'
-                        onClick={this.handleOpen}
-                        color={this.props.zone ? this.props.successColor : undefined}
-                    >
-                        {this.props.zone ? <><Icon name="checkmark"/> Zone is set: {this.props.zone}</> : this.props.message}
-                    </Button>}
-                open={this.state.modalOpen}
-                onClose={this.handleClose}>
-                    <Modal.Header>
-                        {this.props.message}
+  handleOpen = () => {
+    this.setState({
+      modalOpen: true
+    })
+  }
+  handleClose = () => {
+    this.setState({
+      modalOpen: false
+    })
+  }
+  render() {
+    return(
+      <div style={zonePickerStyle}>
+        <Modal 
+        trigger={
+          <Button 
+            fluid size='large'
+            onClick={this.handleOpen}
+            color={this.props.zone ? this.props.successColor : undefined}
+          >
+            {this.props.zone ? <><Icon name="checkmark"/> Zone is set: {this.props.zone}</> : this.props.message}
+          </Button>}
+        open={this.state.modalOpen}
+        onClose={this.handleClose}>
+          <Modal.Header>
+            {this.props.message}
 
-                        <a href="#" style={{ float: 'right' }}>Where&apos;s my zone?</a>
-                    </Modal.Header>
-                    <Modal.Content image>
-                        <Map 
-                        setZoneFromMap={this.props.setZoneFromMap}/>
-                    </Modal.Content>
-                    {this.props.zone && <Modal.Actions>
-                        <Button
-                            primary
-                            fluid
-                            size='large'
-                            onClick={this.handleClose}>
-                            <Icon name="checkmark"/>Done
-                        </Button>
-                    </Modal.Actions>}
-                </Modal>
-            </div>
-        )
-    }
+            <a href="#" style={{ float: 'right' }}>Where&apos;s my zone?</a>
+          </Modal.Header>
+          <Modal.Content image>
+            <Map 
+            setZoneFromMap={this.props.setZoneFromMap}/>
+          </Modal.Content>
+          {this.props.zone && <Modal.Actions>
+            <Button
+              primary
+              fluid
+              size='large'
+              onClick={this.handleClose}>
+              <Icon name="checkmark"/>Done
+            </Button>
+          </Modal.Actions>}
+        </Modal>
+      </div>
+    )
+  }
 }
 
 export default ZonePicker;
